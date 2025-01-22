0088e51ced6080929f86588edef90d885b9f82a3
Adrian Pionka <adrian@pionka.com>
2025-01-22 14:58:51 +0100

    init brewery beer service

---
+++ src/main/java/pl/excellentapp/brewery/beer/BeerApplication.java
@@ -0,0 +1,13 @@
+package pl.excellentapp.brewery.beer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeerApplication.class, args);
+    }
+
+}
