b52ba8823812693c2cb921b9be7a24ae6f14cf6e
Adrian Pionka <adrian@pionka.com>
2025-01-26 10:17:32 +0100

    add GlobalExceptionHandler

--- src/main/java/pl/excellentapp/brewery/beer/infrastructure/rest/api/BeerRestController.java
+++ src/main/java/pl/excellentapp/brewery/beer/infrastructure/rest/api/BeerRestController.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,15 +13,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.excellentapp.brewery.beer.application.BeerService;
-import pl.excellentapp.brewery.beer.domain.exception.BeerNotFoundException;
 import pl.excellentapp.brewery.beer.infrastructure.rest.api.dto.BeerRequest;
 import pl.excellentapp.brewery.beer.infrastructure.rest.api.dto.BeerResponse;
 import pl.excellentapp.brewery.beer.infrastructure.rest.api.dto.BeersResponse;
 import pl.excellentapp.brewery.beer.infrastructure.rest.api.mapper.BeerRestMapper;
 
-import java.util.Date;
-import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.UUID;
 
 @RequiredArgsConstructor
@@ -64,19 +59,6 @@ class BeerRestController {
     public ResponseEntity<HttpStatus> deleteBeer(@PathVariable("beerId") UUID beerId) {
         beerService.delete(beerId);
         return new ResponseEntity<>(HttpStatus.ACCEPTED);
-    }
-
-    @ExceptionHandler(BeerNotFoundException.class)
-    public ResponseEntity<Object> handle(BeerNotFoundException ex) {
-        return handleError(HttpStatus.NOT_FOUND, List.of(ex.getMessage()));
-    }
-
-    private ResponseEntity<Object> handleError(HttpStatus status, List<String> errors) {
-        final var body = new LinkedHashMap<String, Object>();
-        body.put("timestamp", new Date());
-        body.put("status", status.value());
-        body.put("errors", errors);
-        return new ResponseEntity<>(body, status);
     }
 
 }
