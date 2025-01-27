189431fd71acc49c04c8157be793df94be097c0d
Adrian Pionka <adrian@pionka.com>
2025-01-27 12:02:30 +0100

    add http client and rest template builder configuration

--- src/main/java/pl/excellentapp/brewery/beer/BeerApplication.java
+++ src/main/java/pl/excellentapp/brewery/beer/BeerApplication.java
@@ -2,8 +2,10 @@ package pl.excellentapp.brewery.beer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class BeerApplication {
 
     public static void main(String[] args) {
