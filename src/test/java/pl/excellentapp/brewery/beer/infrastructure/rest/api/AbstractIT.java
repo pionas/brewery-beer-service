f10b2593bd43ccb5aef0d4707c27e90a7e7e789d
Adrian Pionka <adrian@pionka.com>
2025-02-23 13:10:43 +0100

    upgrade parent to 1.0.5 version fix tests

--- src/test/java/pl/excellentapp/brewery/beer/infrastructure/rest/api/AbstractIT.java
+++ src/test/java/pl/excellentapp/brewery/beer/infrastructure/rest/api/AbstractIT.java
@@ -4,11 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@Import({RestTemplateConfiguration.class})
-@Profile("it")
+@Import({RestTemplateConfiguration.class, ExternalServicesRestConfiguration.class})
+@ActiveProfiles("it")
 public abstract class AbstractIT {
 
     @Autowired
